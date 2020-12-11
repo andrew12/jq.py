1f90852437f6dc147ea1180faa459b4d733277a8
Andrew Herbig <notandrewherbig@gmail.com>
2020-12-10 21:54:29 -0500

    try again

--- setup.py
+++ setup.py
@@ -1,12 +1,25 @@
 import os
+import subprocess
 
 from Cython.Build import cythonize
 from setuptools import setup
+from setuptools.command.build_ext import build_ext
 from setuptools.extension import Extension
 
 
 def read(fname):
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+
+class jq_build_ext(build_ext):
+    def run(self):
+        commands = [
+            "autoreconf -i",
+            "./configure CFLAGS=-fPIC --disable-maintainer-mode --with-oniguruma=builtin --disable-shared --enable-static --enable-all-static",
+            "make",
+        ]
+        for command in commands:
+            subprocess.check_call(["bash", "-xc", command], cwd="jq")
 
 
 jq_extension = Extension(
@@ -27,6 +40,7 @@ setup(
     python_requires='>=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*',
     license='BSD 2-Clause',
     ext_modules=cythonize([jq_extension]),
+    cmdclass={"build_ext": jq_build_ext},
     classifiers=[
         'Development Status :: 5 - Production/Stable',
         'Intended Audience :: Developers',
